07f3a4de9b875e20cdfa8e51c9b9ffe6179e6313
HanyRabah <hany.rabah@gmail.com>
2025-08-02 00:09:31 +0200

    adding speed insights from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { GoogleAnalytics } from '@/components/GoogleAnalytics'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -63,6 +64,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <GoogleAnalytics />
+        <SpeedInsights />
       </body>
     </html>
   )
